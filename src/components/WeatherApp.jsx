81f8fb3176799102018db36cfdc4dc1d9cfda037
Daniel <sicmylife@gmail.com>
2022-11-17 12:57:35 +0100

    first commit

---
+++ src/components/WeatherApp.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react'
+import React from 'react'
+import WeatherForm from './WeatherForm'
+import WeatherMainInfo from './WeatherMainInfo'
+import styles from './WeatherApp.module.css'
+import Loading from './Loading'
+
+const WeatherApp = () => {
+
+    const [weather, setWeather] = useState(null)
+
+    useEffect(()=>{
+        loadInfo();
+    },[]);
+
+    useEffect(()=>{
+     document.title = `Weather => ${weather?.location.name ?? ""}`
+  },[weather]);
+
+async function loadInfo(city = "london") {
+    await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=9afe2505103b45da9f3120812221611&q=${city}&aqi=no`
+    ).then((res)=> res.json())
+    .then((data) =>{
+      setWeather(data);
+      console.log(data);
+    })
+}
+
+
+    function handleChangeCity(city) {
+        setWeather(null);
+        loadInfo(city);
+      }
+  return (
+    <div className={styles.weatherContainer}>
+<WeatherForm   onChangeCity={handleChangeCity}/>
+{weather ? <WeatherMainInfo weather={weather}/> : <Loading /> }
+
+    </div>
+  )
+}
+
+export default WeatherApp
+
---
+++ src/components/WeatherForm.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useState } from 'react'
+import styles from './WeatherForm.module.css'
+
+
+const WeatherForm = ({ onChangeCity}) => {
+    const [city, setCity]= useState('')
+
+    function onChange(e) {
+        const value = e.target.value;
+    
+        if (value !== "") {
+          setCity(value);
+        }
+      }
+
+      function handleSubmit(e) {
+        e.preventDefault();
+        onChangeCity(city);
+      }
+    
+  return (
+    <div>
+        <form onSubmit={handleSubmit} className={styles.container}>
+            <input type="text" onChange={onChange} className={styles.input} />
+        </form>
+    </div>
+  )
+}
+
+export default WeatherForm
