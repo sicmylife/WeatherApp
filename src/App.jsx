d2528d59e811d0c920f4e146b00d2a3a8ad55f41
Daniel <sicmylife@gmail.com>
2022-11-17 13:03:54 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import './App.css';
-import WeatherApp from './components/WeatherApp';
+import "./App.css";
+import WeatherApp from "./components/WeatherApp";
 
 function App() {
   return (
     <div className="App">
- <WeatherApp/>
+      <WeatherApp />
+
+      <div className="yo">Code by Daniel Daso.</div>
     </div>
   );
 }
